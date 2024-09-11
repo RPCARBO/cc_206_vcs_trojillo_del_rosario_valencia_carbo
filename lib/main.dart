281c164ae08c7eb8e4314b1c0a28a4e281e34ff2
Jed del Rosario <jedandrew.delrosario@wvsu.edu.ph>
2024-09-12 00:42:11 +0800

    Added image and quote.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // 1. Carbo, Ralph Pablo Omahoy: "You can't get to the top by crying! What's the problem? Aim carefully! I'm not dead yet." -Kiryu Kazuma
-// 2. Del Rosario, Jed Andrew Lamayo: " "
+// 2. Del Rosario, Jed Andrew Lamayo: "Come on then, Show me what all the fuss is about!" -Ollanius Piers
 // 3. Trojillo, Cyril Reynold Sucaldito: " "
 // 4. Valencia, Matthew Andrei Octaviano: " "
 
