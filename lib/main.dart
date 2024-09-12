5d222056c83f7a2deaab1030c0b283c324f6be5d
HirokiShinsei <matthewandrei.valencia@wvsu.edu.ph>
2024-09-12 20:08:47 +0800

    Added quote and image

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // 1. Carbo, Ralph Pablo Omahoy: "You can't get to the top by crying! What's the problem? Aim carefully! I'm not dead yet." -Kiryu Kazuma
 // 2. Del Rosario, Jed Andrew Lamayo: "Come on then, Show me what all the fuss is about!" -Ollanius Piers
 // 3. Trojillo, Cyril Reynold Sucaldito: "If you don't take risks, you can't create a future." -Monkey D. Luffy
-// 4. Valencia, Matthew Andrei Octaviano: " "
+// 4. Valencia, Matthew Andrei Octaviano: " To live is to be known and remembered by others." - Himmel
 
 import 'package:flutter/material.dart';
 
